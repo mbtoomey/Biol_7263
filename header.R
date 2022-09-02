66d5c9a40b197e45e73998a728194597ec1872a1
mbtoomey <mbt6332@utulsa.edu>
2022-09-01 22:29:32 -0500

    update lecture2

---
+++ header.R
@@ -0,0 +1,21 @@
+## ---------------------------
+##
+## Script name: 
+##
+## Purpose of script:
+##
+## Author: Matthew Toomey
+##
+## Date Created: 2022-08-30
+##
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+## load up the packages we will need:  
+
+require(tidyverse)
