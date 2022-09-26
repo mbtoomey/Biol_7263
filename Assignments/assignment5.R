a315aa78bbbfd62f6e09c29aefaaab50c1cc7513
Matthew Toomey <mbt6332@utulsa.edu>
2022-09-26 18:33:05 -0500

    add assignment 5

---
+++ Assignments/assignment5.R
@@ -0,0 +1,5 @@
+require(tidyverse)
+
+part1<-read_csv("https://github.com/mbtoomey/Biol_7263/raw/main/Data/assignment6part1.csv")
+
+part2<-read_csv("https://github.com/mbtoomey/Biol_7263/raw/main/Data/assignment6part2.csv")
